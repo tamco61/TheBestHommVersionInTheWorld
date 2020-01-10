d709774707f68526b3132a6be36342c3251f609a
tamco61 <42232243+tamco61@users.noreply.github.com>
2020-01-10 22:04:45 +0500

    main.py V2.0
    2.0

--- main.py
+++ main.py
@@ -2,43 +2,31 @@ import pygame, random, time
 import dop_func
 pygame.init()
 WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
 run = True
-FPS = 75
+FPS = 2
 clock = pygame.time.Clock()
 start = time.time()
 
 
 def start_screen():
-    intro_text = ["PRESS KEY"]
-    fon = pygame.transform.scale(dop_func.load_image(f'fon{str(random.randint(0, 4))}.jpg'), (WIDTH, HEIGHT))
+    intro_text = ["PRESS ANY KEY"]
+    fon = pygame.transform.scale(dop_func.load_image(f'fon{str(random.randint(0, 4))}.jpg'), (screen.get_width(), screen.get_height()))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font("data/cosm.ttf", 50)
     string_rendered = font.render(intro_text[0], 1, pygame.Color('white'))
-    text_coord = [270, 250]
-    napr = 1
-
-    def move_text():
-        global napr
-        speed = 0.3
-        if text_coord[1] <= 250:
-            text_coord[1] += speed
-            napr = 1
-        elif text_coord[1] >= 300:
-            text_coord[1] -= speed
-            napr = 2
-        elif napr == 1:
-            text_coord[1] += speed
-        elif napr == 2:
-            text_coord[1] -= speed
-        screen.blit(string_rendered, (int(text_coord[0]), int(text_coord[1])))
+    t = True
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 dop_func.terminate()
         screen.blit(fon, (0, 0))
-        move_text()
+        if t:
+            screen.blit(string_rendered, (screen.get_width() // 2 - string_rendered.get_width() // 2, screen.get_height() // 4 * 3))
+            t = False
+        else:
+            t = True
         pygame.display.flip()
         clock.tick(FPS)
 
