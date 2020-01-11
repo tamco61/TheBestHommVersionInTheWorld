8809e2828dc89990411ed8b9e0b04d51b7e9a548
slewie <slewie@users.noreply.github.com>
2020-01-11 10:23:57 +0500

    Добавлено стартовое меню игры

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pygame, random, time
 import dop_func
 pygame.init()
-WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
 run = True
 FPS = 2
 clock = pygame.time.Clock()
-start = time.time()
 
 
 def start_screen():
@@ -15,18 +13,44 @@ def start_screen():
     screen.blit(fon, (0, 0))
     font = pygame.font.Font("data/cosm.ttf", 50)
     string_rendered = font.render(intro_text[0], 1, pygame.Color('white'))
-    t = True
+    change_text = True
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 dop_func.terminate()
+            elif event.type == pygame.KEYDOWN:
+                menu()
+                return
         screen.blit(fon, (0, 0))
-        if t:
+        if change_text:
             screen.blit(string_rendered, (screen.get_width() // 2 - string_rendered.get_width() // 2, screen.get_height() // 4 * 3))
-            t = False
+            change_text = False
         else:
-            t = True
+            change_text = True
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def menu():
+    if dop_func.check_save():
+        menu_text = ['Продолжить игру', 'Выйти']
+    else:
+        menu_text = ['Новая игра', 'Выйти']
+    fon = pygame.transform.scale(dop_func.load_image(f'fon{str(random.randint(0, 4))}.jpg'),
+                                 (screen.get_width(), screen.get_height()))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font("data/cosm.ttf", 50)
+    start_game = font.render(menu_text[0], 1, pygame.Color('white'))
+    quit = font.render(menu_text[1], 1, pygame.Color('white'))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                dop_func.terminate()
+            
+        screen.blit(fon, (0, 0))
+        screen.blit(start_game, (screen.get_width() // 2 - start_game.get_width() // 2, screen.get_height() // 4 * 2.5 - 100))
+        screen.blit(quit, (screen.get_width() // 2 - quit.get_width() // 2, screen.get_height() // 4 * 2.5))
         pygame.display.flip()
         clock.tick(FPS)
 
