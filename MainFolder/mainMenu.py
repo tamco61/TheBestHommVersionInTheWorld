912882c95fe79efe46fa443ca74210377ec7e20e
slewie <slewie@users.noreply.github.com>
2020-01-11 17:49:14 +0500

    Добавлены в меню кнопки и смена фона

--- MainFolder/mainMenu.py
+++ MainFolder/mainMenu.py
@@ -1,70 +1,64 @@
-import pygame
+import pygame, random
 from MainFolder import dop_func
 
 pygame.init()
 
 screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
 running = True
-FPS = 2
+FPS = 10
 clock = pygame.time.Clock()
 
 
-class Button(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h, text, on_click=lambda x: None,):
-        self.state = 'normal'
-        self.on_click = on_click
-        pygame.sprite.Sprite.__init__(self)
+class Button:
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.text = text
+        self.inactive_color = (150, 150, 150)
+        self.active_color = (0, 0, 0)
 
-    def draw(self):
-        pygame.draw.rect(surface,self.back_color, self.bounds)
-
-    def handle_mouse_event(self, type, pos):
-        if type == pygame.MOUSEMOTION:
-            self.handle_mouse_move(pos)
-        elif type == pygame.MOUSEBUTTONDOWN:
-            self.handle_mouse_down(pos)
-        elif type == pygame.MOUSEBUTTONUP:
-            self.handle_mouse_up(pos)
-
-    def handle_mouse_move(self, pos):
-        if self.bounds.collidepoint(pos):
-            if self.state != 'pressed':
-                self.state = 'hover'
+    def draw(self, x, y, text, action=None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x < mouse[0] < x + self.width:
+            if y < mouse[1] < y + self.height:
+                pygame.draw.rect(screen, self.active_color, (x, y, self.width, self.height))
+                if click[0] == 1 and action is not None:
+                    action()
+            else:
+                pygame.draw.rect(screen, self.inactive_color, (x, y, self.width, self.height))
         else:
-            self.state = 'normal'
+            pygame.draw.rect(screen, self.inactive_color, (x, y, self.width, self.height))
 
-    def handle_mouse_down(self, pos):
-        if self.bounds.collidepoint(pos):
-            self.state = 'pressed'
+        print_text(text, x + 10, y + 15)
 
-    def handle_mouse_up(self, pos):
-        if self.state == 'pressed':
-            self.on_click(self)
-            self.state = 'hover'
+
+def print_text(text, x, y, font_size=50):
+    font_type = pygame.font.Font("data/cosm.ttf", font_size)
+    mes = font_type.render(text, 1, pygame.Color('white'))
+    screen.blit(mes, (x, y))
 
 
 def menu():
-    if dop_func.check_save():
-        menu_text = ['Продолжить игру', 'Выйти']
-    else:
-        menu_text = ['Новая игра', 'Выйти']
     fon = pygame.transform.scale(dop_func.load_image(f'fon{str(random.randint(0, 4))}.jpg'),
                                  (screen.get_width(), screen.get_height()))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font("data/cosm.ttf", 50)
-    start_game = font.render(menu_text[0], 1, pygame.Color('white'))
-    quit = font.render(menu_text[1], 1, pygame.Color('white'))
+    start_game = Button(350, 80)
+    quit = Button(225, 80)
+    counter_sec = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 dop_func.terminate()
-
+        counter_sec += 1
+        if counter_sec == 40:
+            fon = pygame.transform.scale(dop_func.load_image(f'fon{str(random.randint(0, 4))}.jpg'),
+                                         (screen.get_width(), screen.get_height()))
+            counter_sec = 0
         screen.blit(fon, (0, 0))
-        screen.blit(start_game,
-                    (screen.get_width() // 2 - start_game.get_width() // 2, screen.get_height() // 4 * 2.5 - 100))
-        screen.blit(quit, (screen.get_width() // 2 - quit.get_width() // 2, screen.get_height() // 4 * 2.5))
+        start_game.draw(screen.get_width() // 2 - start_game.width // 2, screen.get_height() // 4 * 2.5 - 200, 'Новая игра')
+        quit.draw(screen.get_width() // 2 - quit.width // 2, screen.get_height() // 4 * 2.5 - 100, 'Выйти')
         pygame.display.flip()
         clock.tick(FPS)
+
+
