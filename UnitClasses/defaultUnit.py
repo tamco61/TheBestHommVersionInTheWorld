107f9d1c2bee421333e687362a2aad356d2b4a13
rrr <tamco@list.ru>
2020-01-12 13:13:00 +0500

    Дополнил класс HeroUnit и создал Group. Имена функций 'говорящие'.

--- UnitClasses/defaultUnit.py
+++ UnitClasses/defaultUnit.py
@@ -1,36 +1,60 @@
-organization_lst = ['Sith', 'Neutral', 'Jedi Order']
+class Group:
+    def __init__(self, name):
+        self.name = name
+        self.lst = list()
+
+    def get_bonus(self, hero):
+        bHP = 0
+        bDMG = 0
+        bARMOUR = 0
+        for i in self.lst:
+            if i.name() == hero.name():
+                continue
+            bHP += i.bHP()
+            bDMG += i.bDMG()
+            bARMOUR += i.bARMOUR()
+        return bHP, bDMG, bDMG
+
+    def append_hero(self, hero):
+        if len(self.lst) < 5:
+            self.lst.append(hero)
+
+    def remove_hero(self, index):
+        self.lst.remove(index)
+
+    def lst(self):
+        return self.lst
 
 
 # Базовый класс
 class DefaultUnit:
-    def __init__(self, hp, damage, armour, attack_speed):
+    def __init__(self, id, name, hp, damage, armour):
+        self.id = id
+        self.name = name
         self.hp = hp
         self.dmg = damage
         self.armour = armour
-        self.ats = attack_speed
 
-    def take_damage(self, damage):
-        self.hp -= int(damage * (self.armour / (self.armour + 10 * damage)))
+    def name(self):
+        return self.name
 
 
-# Базовый класс командующего
-class CommanderUnit(DefaultUnit):
-    def __init__(self, hp, damage, armour, attack_speed, organization=organization_lst[1], bonus_hp=0, bonus_damage=0,
-                 bonus_armour=0, bonus_attack_speed=0):
-        super().__init__(hp, damage, armour, attack_speed)
-        self.group = list()
-        self.org = organization
+# Базовый класс героя
+class HeroUnit(DefaultUnit):
+    def __init__(self, id,  name, hp, damage, armour, group, bonus_hp=0, bonus_damage=0, bonus_armour=0):
+        super().__init__(id, name, hp, damage, armour)
+        self.group = group
         self.bHP = bonus_hp
         self.bDMG = bonus_damage
         self.bARMOUR = bonus_armour
-        self.bATS = bonus_attack_speed
-        self.org = organization
 
     def take_damage(self, damage):
-        self.hp -= int(damage * (self.armour / (self.armour + 10 * damage)))
+        h, d, a = self.group.get_bonus()
+        self.hp -= h + int(damage * ((self.armour + a) / (self.armour + a + 10 * damage)))
 
-    def add_group(self, unit):
-        self.group.append(unit)
+    def get_damage(self):
+        h, d, a = self.group.get_bonus()
+        return self.dmg + d
 
     def bHP(self):
         return self.bHP
@@ -41,23 +65,5 @@ class CommanderUnit(DefaultUnit):
     def bARMOUR(self):
         return self.bARMOUR
 
-    def bATS(self):
-        return self.bATS
-
-    def org(self):
-        return self.org
-
-
-# Базовый класс солдата
-class SoldierUnit(DefaultUnit):
-    def __init__(self, hp, damage, armour, attack_speed, organization=organization_lst[1], commander=None):
-        super().__init__(hp, damage, armour, attack_speed)
-        self.org = organization
-        self.com = commander
-
-    def take_damage(self, damage):
-        if self.com is None:
-            self.hp -= int(damage * (self.armour / (self.armour + 10 * damage)))
-        else:
-            self.hp = self.hp + self.com.bHP() - int(damage * ((self.armour + self.com.bARMOUR()) / ((self.armour + self.com.bARMOUR()) + 10 * damage)))
-
+    def id(self):
+        return self.id
