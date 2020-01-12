ee96c38b39a0f207899b90ae2ce4835c88cf5850
slewie <slewie@users.noreply.github.com>
2020-01-12 22:08:50 +0500

    Все бд объеденены в одну, лежащую в папке data

--- MainFolder/database.py
+++ MainFolder/database.py
@@ -6,9 +6,9 @@ CURSOR = CONNECTION.cursor()
 
 def take_hero(name=0):
     if name:
-        person = CURSOR.execute('SELECT name, hp, dmg, arm, organization FROM heroes WHERE (name = ?)',
+        person = CURSOR.execute('SELECT name, hp, dmg, arm, orm FROM hero WHERE (name = ?)',
                                 (name, )).fetchone()
     else:
-        person = CURSOR.execute('SELECT name, hp, dmg, arm, organization FROM heroes WHERE (id = ?)',
+        person = CURSOR.execute('SELECT name, hp, dmg, arm, orm FROM hero WHERE (id = ?)',
                                 (random.randint(1, 6),)).fetchone()
     return person
