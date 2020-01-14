d47123bca3b38d466dd5ce8ce5f9dc71e494c80d
slewie <slewie@users.noreply.github.com>
2020-01-14 19:44:05 +0500

    Сделан показ стат, но лучше как-нибудь увеличить скорость, коробль развернут

--- MainFolder/mainMenu.py
+++ MainFolder/mainMenu.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
 running = True
 FPS = 10
 clock = pygame.time.Clock()
-
+WIDTH, HEIGHT = screen.get_width(), screen.get_height()
 pygame.mixer.music.load('data/mus1.mp3')
 
 
@@ -55,12 +55,24 @@ def set_captain(name):
     return
 
 
+def draw_stat(lst, lst1):
+    print_text(lst[0], lst1[1][0] + WIDTH // 20, lst1[1][1] + HEIGHT // 6, font_size=25, color='black',
+               font_type='stat.ttf')
+    print_text(f'Hit Point {lst[1]}', lst1[1][0] + WIDTH // 20, lst1[1][1] + HEIGHT // 6 + 35, font_size=25,
+               color='black', font_type='stat.ttf')
+    print_text(f'Damage {lst[2]}', lst1[1][0] + WIDTH // 20, lst1[1][1] + HEIGHT // 6 + 70, font_size=25,
+               color='black', font_type='stat.ttf')
+    print_text(f'Armour {lst[3]}', lst1[1][0] + WIDTH // 20, lst1[1][1] + HEIGHT // 6 + 105, font_size=25,
+               color='black', font_type='stat.ttf')
+    print_text(f'{lst[4]}', lst1[1][0] + WIDTH // 20, lst1[1][1] + HEIGHT // 6 + 140, font_size=25,
+               color='black', font_type='stat.ttf')
+
+
 def menu():
     fon = pygame.transform.scale(dop_func.load_image(f'fon{str(random.randint(0, 4))}.jpg'),
                                  (screen.get_width(), screen.get_height()))
-
     start_game = Button(350, 80)
-    quit = Button(225, 80)
+    quit_button = Button(225, 80)
     counter_sec = 0
     pygame.mixer.music.play(-1)
     while True:
@@ -75,7 +87,8 @@ def menu():
         screen.blit(fon, (0, 0))
         start_game.draw(screen.get_width() // 2 - start_game.width // 2,
                         screen.get_height() // 4 * 2.5 - 200, 'Новая игра', choose_captain)
-        quit.draw(screen.get_width() // 2 - quit.width // 2, screen.get_height() // 4 * 2.5 - 100, 'Выйти', quit)
+        quit_button.draw(screen.get_width() // 2 - quit_button.width // 2, screen.get_height() // 4 * 2.5 - 100,
+                         'Выйти', quit)
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -86,32 +99,48 @@ def choose_captain():
     heroes = set()
     while len(heroes) != 5:
         heroes.add(database.take_hero())
-    FPS = 60
     heroes = list(heroes)
     image0 = pygame.transform.scale(dop_func.load_image("heroes/" + heroes[0][-1]),
-                                    (screen.get_width() // 6, screen.get_height() // 2))
+                                    (5 * screen.get_width() // 26, screen.get_height() // 2))
     image1 = pygame.transform.scale(dop_func.load_image("heroes/" + heroes[1][-1]),
-                                    (screen.get_width() // 6, screen.get_height() // 2))
+                                    (5 * screen.get_width() // 26, screen.get_height() // 2))
     image2 = pygame.transform.scale(dop_func.load_image("heroes/" + heroes[2][-1]),
-                                    (screen.get_width() // 6, screen.get_height() // 2))
+                                    (5 * screen.get_width() // 26, screen.get_height() // 2))
     image3 = pygame.transform.scale(dop_func.load_image("heroes/" + heroes[3][-1]),
-                                    (screen.get_width() // 6, screen.get_height() // 2))
+                                    (5 * screen.get_width() // 26, screen.get_height() // 2))
     image4 = pygame.transform.scale(dop_func.load_image("heroes/" + heroes[4][-1]),
-                                    (screen.get_width() // 6, screen.get_height() // 2))
-    dct_names = {0: heroes[0][0], 1: heroes[1][0], 2: heroes[2][0], 3: heroes[3][0], 4: heroes[4][0]}
+                                    (5 * screen.get_width() // 26, screen.get_height() // 2))
+    dct_names = {0: [heroes[0][0]], 1: [heroes[1][0]], 2: [heroes[2][0]], 3: [heroes[3][0]], 4: [heroes[4][0]]}
     choose = Button(185, 65, text_size=30)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 dop_func.terminate()
         screen.fill((255, 255, 255))
-        print_text('Выберите капитана', (2 * screen.get_width()) // 6, screen.get_height() // 8, color='black')
+        print_text('Выберите капитана', (2 * WIDTH) // 6, HEIGHT // 8, color='black')
         for i in range(0, 5):
-            screen.blit(locals()['image%s' % i], (screen.get_width() // 8 + 0.9 * i * screen.get_width() // 6,
-                                                  screen.get_height() // 5))
-            choose.draw(screen.get_width() // 8 + i * screen.get_width() // 6, 3 * screen.get_height() // 4, "Выбрать",
+            choose.draw(3 * WIDTH // 30 + i * WIDTH // 6, 3 * HEIGHT // 4, "Выбрать",
                         set_captain, dct_names[i])
-
+            if len(dct_names[i]) == 1:
+                dct_names[i] = [dct_names[i][0], (3 * WIDTH // 50 + 8 * i * image0.get_width() // 9,
+                                                  HEIGHT // 5),
+                                (3 * WIDTH // 50 + 8 * i * image0.get_width() // 9 + image0.get_width(),
+                                 HEIGHT // 5 + HEIGHT // 2), False]
+            if dct_names[i][-1] is False:
+                dct_names[i] = [dct_names[i][0], (3 * WIDTH // 50 + 8 * i * image0.get_width() // 9,
+                                                  HEIGHT // 5),
+                                (3 * WIDTH // 50 + 8 * i * image0.get_width() // 9 + image0.get_width(),
+                                 HEIGHT // 5 + HEIGHT // 2), False]
+                screen.blit(locals()['image%s' % i], (3 * WIDTH // 50 + 8 * i * image0.get_width() // 9, HEIGHT // 5))
+            else:
+                dct_names[i] = [dct_names[i][0], (3 * WIDTH // 50 + 8 * i * image0.get_width() // 9,
+                                                  HEIGHT // 5),
+                                (3 * WIDTH // 50 + 8 * i * image0.get_width() // 9 + image0.get_width(),
+                                 HEIGHT // 5 + HEIGHT // 2), True]
+                draw_stat(heroes[i], dct_names[i])
+            if dct_names[i][1][0] <= pygame.mouse.get_pos()[0] <= dct_names[i][2][0] and\
+                    dct_names[i][1][1] <= pygame.mouse.get_pos()[1] <= dct_names[i][2][1] and pygame.mouse.get_pressed()[0]:
+                dct_names[i][-1] = not dct_names[i][-1]
         pygame.display.flip()
         clock.tick(FPS)
 
